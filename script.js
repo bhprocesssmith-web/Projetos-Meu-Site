626fdec32b054c5882770ebfc1c22d16782ff3a1
bhprocesssmith-web <bhprocesssmith@gmail.com>
2026-03-17 05:47:34 -0300

    commit size screen

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function clicar(){
-    alert("botao clicado");
+function mostrar(secao) {
+    const conteudo = document.getElementById("conteudo");
+
+    if (secao === "home") {
+        conteudo.innerHTML = "<h2>Home</h2><p>Página inicial</p>";
+    }
+    else if (secao === "jogo") {
+        conteudo.innerHTML = "<h2>Jogo</h2><p>Iniciar jogo aqui</p>";
+    }
+    else if (secao === "config") {
+        conteudo.innerHTML = "<h2>Configurações</h2><p>Ajustes do sistema</p>";
+    }
 }
